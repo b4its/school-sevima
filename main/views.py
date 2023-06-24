c2a4b26863eaa6fccb0f6cdd1155d3419e2560d0
b4its <exynosfam@gmail.com>
2023-06-24 10:38:09 +0800

    menambahkan dan mengatur home base template dari main app

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+def index(request):
+    return render(request, 'index.html')
